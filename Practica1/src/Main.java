333c9c8ce3b42b5960cb59f8d61fa8461f0002ab
José Góngora <jdavidgongorao@gmail.com>
2023-01-31 15:03:00 -0600

    Se agregó el menu

--- Practica1/src/Main.java
+++ Practica1/src/Main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String user = "cajero_202201444";
         String password = "ipc1_202201444";
         String userL = "";
         String passL = "";
+        String option = "";
+        boolean flag;
         System.out.println("\n" +
                 "   _____ _    _ _____  ______ _____      ___  _____ \n" +
                 "  / ____| |  | |  __ \\|  ____|  __ \\    |__ \\| ____|\n" +
@@ -17,7 +19,7 @@ public class Main {
                 "                                                    \n" +
                 "                                                    \n");
 
-
+        // LOGIN
         while (!userL.equals(user) || !passL.equals(password)) {
             System.out.println("======= POR FAVOR INGRESAR TUS CREDENCIALES =======");
             System.out.println("INGRESAR USUARIO:");
@@ -27,7 +29,56 @@ public class Main {
             if(!userL.equals(user) && !passL.equals(password)) {
                 System.out.println("DATOS INCORRECTOS");
             }
-        } // LOGIN
+        }
+        System.out.println("======= BIENVENIDO USUARIO "+user+" =======\n");
 
+        // MENU
+        do {
+            System.out.println("======= MENU =======");
+            System.out.println("| 1 | AGREGAR PRODUCTOS");
+            System.out.println("| 2 | AGREGAR CUPONES DE DESCUENTO");
+            System.out.println("| 3 | REALIZAR VENTAS");
+            System.out.println("| 4 | GENERAR REPORTES");
+            System.out.println("INGRESE UNA OPCIÓN");
+            option = sc.nextLine();
+            flag = true;
+            switch (option) {
+                case "1":
+                    System.out.println("======= AGREGAR PRODUCTOS =======");
+                    AgregarProductos();
+                    break;
+                case "2":
+                    System.out.println("======= AGREGAR CUPONES DE DESCUENTO =======");
+                    AgregarCupones();
+                    break;
+                case "3":
+                    System.out.println("======= REALIZAR VENTAS =======");
+                    RealizarVentas();
+                    break;
+                case "4":
+                    System.out.println("======= GENERAR REPORTES =======");
+                    GenerarReportes();
+                    break;
+                default:
+                    flag = false;
+                    System.out.println("OPCIÓN NO VALIDA\n");
+            }
+        } while(flag == false);
+
+    }
+
+    public static void AgregarProductos() {
+        System.out.println("Metodo agregar productos");
+    }
+    public static void AgregarCupones() {
+        System.out.println("Metodo agregar cupones");
+    }
+
+    public static void RealizarVentas() {
+        System.out.println("Metodo realizar ventas");
+    }
+
+    public static void GenerarReportes() {
+        System.out.println("Metodo generar reportes");
     }
 }
